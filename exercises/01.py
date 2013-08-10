ccb2a2b960ebf87151804b4c753b05521f12aa95
Elliot Voris <elliotfriend@gmail.com>
2013-08-09 22:41:48 -0500

    completed first exercise

---
+++ exercises/01.py
@@ -0,0 +1,8 @@
+# This URL: http://www.pythonchallenge.com/pc/def/map.html
+# Next URL: 
+#
+# We need to map one sentence a couple letters over. Here's our clue:
+# K -> M
+# O -> Q
+# E -> G
+# "everybody thinks twice before solving this."
