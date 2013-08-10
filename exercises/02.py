f96238df365058d2013151ea6d4db51c21fa9cc9
Elliot Voris <elliotfriend@gmail.com>
2013-08-09 23:16:03 -0500

    forgot the next url

--- exercises/02.py
+++ exercises/02.py
@@ -1,14 +1,7 @@
 # This URL: http://www.pythonchallenge.com/pc/def/ocr.html
-# Next URL: 
+# Next URL: http://www.pythonchallenge.com/pc/def/equality.html
 import string
 # view the page source to get to the real part of the riddle
-f = open('/home/elliot/Development/pythonchallenge.com/data/02.txt', 'r')
-alphanumeric = string.ascii_lowercase + str('0123456789')
-letters_and_numbers = []
-print alphanumeric
-for line in f:
-    for letter in line:
-        if letter in alphanumeric:
-            letters_and_numbers.append(letter)
-            
-print letters_and_numbers
+text = open('/home/elliot/Development/pythonchallenge.com/data/02.txt', 'r').read()
+
+print filter(lambda x: x in string.letters, text)
