e6a8d5fbb929ee823f80a48d2fe6116e7eb780e1
Elliot Voris <elliotfriend@gmail.com>
2013-08-09 23:38:48 -0500

    completed third exercise

---
+++ exercises/03.py
@@ -0,0 +1,13 @@
+# This URL: http://www.pythonchallenge.com/pc/def/equality.html
+# Next URL: http://www.pythonchallenge.com/pc/def/linkedlist.html
+
+import re
+
+answer = ""
+
+text = open("/home/elliot/Development/pythonchallenge.com/data/03.txt", "r")
+for i in re.findall('[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]', text.read()):
+    answer += i[len(i) // 2]
+
+print answer
+text.close()
