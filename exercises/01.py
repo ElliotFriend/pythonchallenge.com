92b6be9d6c35a158e865e873fe8d1f4480b50c71
Elliot Voris <elliotfriend@gmail.com>
2013-08-09 22:54:47 -0500

    really completed the first exercise

--- exercises/01.py
+++ exercises/01.py
@@ -1,8 +1,26 @@
+import string
+
 # This URL: http://www.pythonchallenge.com/pc/def/map.html
-# Next URL: 
+# Next URL: http://www.pythonchallenge.com/pc/def/ocr.html
 #
 # We need to map one sentence a couple letters over. Here's our clue:
 # K -> M
 # O -> Q
 # E -> G
 # "everybody thinks twice before solving this."
+# ABCDEFGHIJKLMNOPQRSTUVWXYZ
+#     | |   | | | |
+#     ---   --- ---
+# Here's the text to transcode:
+text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq
+ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq
+qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
+
+alphabet = string.ascii_lowercase[:26]
+code = string.ascii_lowercase[2:] + string.ascii_lowercase[:2]
+trans = string.maketrans(alphabet, code)
+url = "map"
+
+
+print text.translate(trans)
+print url.translate(trans)
